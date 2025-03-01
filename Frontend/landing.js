f834e0c377d00cda923d312da5ffaf3b2a8a7afc
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2025-03-01 23:52:07 +0530

    Replacd with deployed link

--- Frontend/landing.js
+++ Frontend/landing.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('loginPassword').value;
 
             try {
-                const response = await fetch('http://localhost:5000/api/login', {
+                const response = await fetch('https://backend-l84813nn2-bibhabasu-debnaths-projects.vercel.app/api/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -149,7 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch('http://localhost:5000/api/register', {
+                const response = await fetch('https://backend-l84813nn2-bibhabasu-debnaths-projects.vercel.app/api/register', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
