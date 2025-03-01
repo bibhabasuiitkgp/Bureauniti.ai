f5fdec60e8760264bfabe05ecaa3c54bab70e44f
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2025-03-02 00:18:49 +0530

    Code

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -10,7 +10,7 @@ const saltRounds = 10;
 
 // Middleware
 app.use(cors({
-    origin: 'https://bureauniti-ai-pcqp.vercel.app',  // Your frontend URL
+    origin: 'http://127.0.0.1:5501/Frontend',  // Your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
